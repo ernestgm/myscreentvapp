85223e23e759e16fcca545dd755309a57aed682f
Ernesto Gonzalez <ergonzalez@hg.com.uy>
2024-09-25 12:59:55 -0400

    Services and logs files tracking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -169,6 +169,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
-
+    implementation(libs.firebase.perf)
     implementation (libs.commons.net)
 }
